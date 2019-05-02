933d745bbb86d84f6ca8841846ed6fdb3a17c0a6
Tarmstrong95 <triston95strong@gmail.com>
2019-05-02 16:07:08 -0500

    Style Seachbar

--- instagram-app/src/components/SearchBar/searchbar.jsx
+++ instagram-app/src/components/SearchBar/searchbar.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import './searchbar.css';
-import {Nav} from '../styledComponents/styledComponents';
+import {Nav, IconContainer, SmIcon, NmIcon, Search} from '../styledComponents/styledComponents';
 
 const SearchBar = (props) => {
     return (
         <Nav>
-            <div className="icon-cont">
-                <div className="camera sm-ico"/>
-                <div className="name"/>
-            </div>
+            <IconContainer >
+                <SmIcon camera />
+                <NmIcon />
+            </IconContainer>
             <form>
-                <input className="search-inp" onChange={props.search} placeholder="Search" />
+                <Search onChange={props.search} placeholder="Search" />
             </form>
-            <div className="icon-cont">
-                <div className="compass sm-ico"  />
-                <div className="heart sm-ico"  />
-                <div className="person sm-ico"  />
-            </div>
+            <IconContainer >
+                <SmIcon compass />
+                <SmIcon heart />
+                <SmIcon person />
+            </IconContainer>
         </Nav>
     )
 }
--- instagram-app/src/components/styledComponents/styledComponents.jsx
+++ instagram-app/src/components/styledComponents/styledComponents.jsx
@@ -1,15 +1,21 @@
 // All styled components
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import camera from '../../Icons/instagram-line-icon.png'
+import compass from '../../Icons/instagram-line-compass.png'
+import heart from '../../Icons/instagram-line-heart.png'
+import person from '../../Icons/instagram-line-person.png'
+import name from '../../Icons/instagram.png'
 
+//============ Nav styles ============
 export const Nav = styled.div`
-display: flex;
-width: 100%;
-max-width: 1010px;
-padding: 26px 20px;
-justify-content: space-between;
-align-items: center;
-margin: auto;
-border-bottom: 1px solid rgba(0,0,0,.0975);
+    display: flex;
+    width: 100%;
+    max-width: 1010px;
+    padding: 26px 20px;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+    border-bottom: 1px solid rgba(0,0,0,.0975);
 `;
 
 export const IconContainer = styled.div`
@@ -17,7 +23,59 @@ display: flex;
 justify-content: space-between;
 `;
 
-const Notacomponent = () => {
-    return(null)
+export const SmIcon = styled.div`
+    width: 24px;
+    height: 24px;
+    background-repeat: no-repeat;
+    margin-right: 20px;
+
+${props => props.camera && css`background-image: url(${camera});`}
+${props => props.compass && css`background-image: url(${compass});`}
+${props => props.heart && css`background-image: url(${heart});`}
+${props => props.person && css`background-image: url(${person});`}
+`;
+
+export const NmIcon = styled.div`
+    background-image: url(${name});
+    width: 103px;
+    height: 29px;
+    background-repeat: no-repeat;
+`
+
+export const Search = styled.input`
+    border-radius: 4px;
+    padding: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.377);
+    margin: auto;
+`
+
+// =========== Post container styles ===========
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Notarealcomponent = () => {
+    return (null)
 }
-export default Notacomponent;
+export default Notarealcomponent;
